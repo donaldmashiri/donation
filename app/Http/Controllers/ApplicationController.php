ba175be768e7db7edd2bc9f6c86d7de8af2b5ed1
donaldmashiri <donaldtondemashiri@gmail.com>
2023-06-08 04:02:58 +0200

    imagev

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -33,7 +33,7 @@ class ApplicationController extends Controller
         $request->validate([
             'full_names' => ['required', 'max:255'],
             'email' => ['required', 'max:255'],
-            'image' => ['required|image|mimes:jpeg,png,jpg,gif,svg'],
+            'image' => ['required', 'image', 'max:255', 'mimes:png,jpg,jpeg'],
             'app_type' => ['required', 'max:255'],
             'amount' => ['required', 'max:255'],
             'comments' => ['required'],
